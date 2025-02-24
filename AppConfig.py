f37e627f428f9d8a00f83d65d4e78bf7d213a21f
Andy Shaw <shaw.andy.p@gmail.com>
2025-02-24 12:46:04 -0800

    Update AppConfig.py

--- AppConfig.py
+++ AppConfig.py
@@ -1,6 +1,6 @@
 
 APPLICATION_NAME = "Stega Editor"
 
-MAX_SIZE = 250 # either dimension, keep aspect ratio when resizing
+MAX_SIZE = 500 # either dimension, keep aspect ratio when resizing
 
-DEFAULT_IMAGE_PATH = "C:\\Users\\Andy\\Documents\\Programming\\Python\\pwm\\image.png"
+DEFAULT_IMAGE_PATH = "C:\\Users\\Andy\\Documents\\Programming\\Python\\pwm\\test-data\\image.png"
