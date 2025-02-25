9bb2c4fa9f61df5ed6c8e46435a8820cdecece87
Andy Shaw <shaw.andy.p@gmail.com>
2025-02-25 11:05:03 -0800

    remove config value

--- AppConfig.py
+++ AppConfig.py
@@ -2,5 +2,3 @@
 APPLICATION_NAME = "Stega Editor"
 
 MAX_SIZE = 500 # either dimension, keep aspect ratio when resizing
-
-DEFAULT_IMAGE_PATH = "C:\\Users\\Andy\\Documents\\Programming\\Python\\pwm\\test-data\\image.png"
